ff70bf388ed336db9638a15e5efe878dbeebe82a
yoshida-y1-madoka <yoshida-y1-madoka@jna.co.jp>
2025-02-13 18:51:44 +0900

    handson commit

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-1_gKvsKhUiY', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '145v2gjkvh464lg5dr237jibg7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7jnitt2v83tq84h7blltqrtujb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
